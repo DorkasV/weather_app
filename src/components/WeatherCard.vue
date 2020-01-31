9bb13de84d22b5c316167bf7fd45ae06edb6a6fd
DorkasV <neturiu.kaipgaila@gmail.com>
2020-01-31 09:25:07 +0200

    Country flag added

--- src/components/WeatherCard.vue
+++ src/components/WeatherCard.vue
@@ -2,14 +2,17 @@
   <b-row v-if="info" align-h="center">
     <b-col cols="4">
       <b-card
-        :title="`${(info||{}).name}, ${((info||{}).sys||{}).country} ${((info||{}).weather||{})[0].description}`"
         tag="article"
         style="max-width: 30rem;"
         class="mb-2"
         @click="$emit('cityDetails', info.id)"
       >
-        <b-card-text>
+        <b-card-title>
+          {{(info||{}).name}}, {{((info||{}).sys||{}).country}} 
           <img alt="Flag" :src="getImageUrl(info.sys.country)">
+          {{((info||{}).weather||{})[0].description}}
+        </b-card-title>
+        <b-card-text>
           <b-badge variant="info">{{`${((info||{}).main||{}).temp} &#176;C`}}</b-badge>
           {{`temperature from ${((info||{}).main||{}).temp_min} to ${((info||{}).main||{}).temp_max} &#176;C, 
           wind ${((info||{}).wind||{}).speed} m/s. clouds ${((info||{}).clouds||{}).all} %`}}
